6aa18ec97d67b204291d8b4b5620fea7775830ca
Govardhan <saigov14@gmail.com>
2023-05-03 14:34:51 +0530

    ALU logic modification

--- design/ALU.v
+++ design/ALU.v
@@ -1,51 +1,47 @@
-`timescale 1ns / 1ps
-/*
- * Copyright (c) 2023 Govardhan
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
+` timescale 1 ns / 1 ps
+  /*
+   * Copyright (c) 2023 Govardhan
+   *
+   * Permission is hereby granted, free of charge, to any person obtaining a copy
+   * of this software and associated documentation files (the "Software"), to deal
+   * in the Software without restriction, including without limitation the rights
+   * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+   * copies of the Software, and to permit persons to whom the Software is
+   * furnished to do so, subject to the following conditions:
+   *
+   * The above copyright notice and this permission notice shall be included in
+   * all copies or substantial portions of the Software.
+   *
+   * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+   * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+   * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+   * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+   * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+   * THE SOFTWARE.
+   */
+  module ALU (input wire[31: 0] A, B, input wire[2: 0] ALUControl, output wire Zero, output wire[31:0] Result);
 
-module ALU(
-	   input wire [31:0]  A,B,
-	   input wire [2:0]   ALUControl,
-	   output wire 	      Zero,
-	   output wire [31:0] Result );
+   reg[31:0] ResultReg;
+   wire [31:0] temp1, Sum;
+   wire	       V, slt;			//overflow
 
-   reg [31:0] 		      ResultReg;
-   wire [31:0] 		      temp1,Sum;
-   wire 		      V,slt; //overflow
-
-   assign temp1 = ALUControl[0] ? ~B:B;
+   assign temp1 = ALUControl[0] ? ~B : B;
    assign Sum = A + temp1 + ALUControl[0];
-   assign V = ((~ALUControl[1]) & (A[31]^Sum[31]) & (~ALUControl[0]^A[31]^B[31]));
-   assign slt = V^Sum[31];
+   assign V =
+	     ((~ALUControl[1]) & (A[31] ^ Sum[31]) & (~ALUControl[0] ^ A[31] ^ B[31]));
+   assign slt = V ^ Sum[31];
 
-   always@(*)
-     case(ALUControl)
-       3'b000: ResultReg <= Sum; //add
-       3'b001: ResultReg <= Sum; //sub
-       3'b010: ResultReg <= A&B; //and
-       3'b011: ResultReg <= A|B; //or
-       3'b101: ResultReg <= {31'b0,slt}; //slt
-     endcase
+   always @ (*)case (ALUControl) 3 'b000: ResultReg <= Sum; //add
+		 3' b001:
+		   ResultReg <= Sum;		//sub
+		 3 'b010: ResultReg <= A&B; //and
+		 3' b011:ResultReg <= A | B;
+		 //or
+		 3 'b101: ResultReg <= {31' b0, slt};	//slt
 
-   assign Zero = (ResultReg == 32'b0);
+	       endcase assign Zero = (ResultReg == 32 'b0);
    assign Result = ResultReg;
 
 endmodule
+
--- design/ALU_decoder.v
+++ design/ALU_decoder.v
@@ -21,20 +21,20 @@
  * THE SOFTWARE.
  */
 module ALU_Decoder(
-		   input wire 	    opb5,
+		   input wire	    opb5,
 		   input wire [2:0] funct3,
-		   input wire 	    funct7b5,
+		   input wire	    funct7b5,
 		   input wire [1:0] ALUOp,
 		   output reg [2:0] ALUControl
 		   );
 
-   wire 			    RtypeSub;
+   wire				    RtypeSub;
    assign RtypeSub = funct7b5 & opb5; //TRUE for R-type substract
 
    always@(*)
      begin
 	case(ALUOp)
-          2'b00:	ALUControl = 3'b000; //addition
+          2'b00:  ALUControl = 3'b000; //addition
           2'b01:  ALUControl = 3'b001; //subtraction
           default: 
             case(funct3)//R-type or I-type ALU
